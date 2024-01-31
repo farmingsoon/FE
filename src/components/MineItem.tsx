87fd9ac2c78df90a2dbc1a814f035a82e9a2d06a
Emma-Hyejin <cjd191058@naver.com>
2024-02-01 01:59:55 +0900

    Amend: MineItem 컴포넌트 배포 에러 사항 수정

--- src/components/MineItem.tsx
+++ src/components/MineItem.tsx
@@ -13,12 +13,17 @@ interface MineItemTypes {
 }
 
 const MineItem = ({type, data}: MineItemTypes) => {
+
+    const formatPrice = (price: number) => {
+        return String(price).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+
     return(
         <div className="my-2 flex flex-row whitespace-nowrap">
-            <div className="w-24 h-24 mr-3 "><Img src={null} type={"normal"}/></div>
+            <div className="overflow-hidden mr-3 "><Img src={null} type={"normal"} width={96} height={96}/></div>
             <div className="flex-1 flex flex-col text-base  py-3 justify-around min-w-64">
                 <div>{data.title}</div>
-                <div className="text-sm font-light">현재최고가<span className="text-POINT_BLUE font-semibold"> ₩ {data && String(data.price).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span> 원</div>
+                <div className="text-sm font-light">현재최고가<span className="text-POINT_BLUE font-semibold"> ₩ {data && formatPrice(data.price)}</span> 원</div>
                 <div className="text-xs font-light ">서울 ・ 3일 2시간 남음</div>
             </div>
             {type === "bidded" 
