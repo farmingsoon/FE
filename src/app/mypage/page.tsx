cfed50ce5f0c5274c7ba8ee7958c949def87dcb2
Emma-Hyejin <cjd191058@naver.com>
2024-02-05 21:51:47 +0900

    Feat: 마이페이지 판매 상품과 경매 상품 구분

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,6 +3,7 @@ import Img from "@/common/Img";
 import MineItem from "@/components/MineItem";
 import LocalStorage from "@/util/localstorage";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 interface MypageTypes {
@@ -21,26 +22,43 @@ interface MypageTypes {
 export default function Login() {
     const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
     const accessToken = LocalStorage.getItem("accessToken");
-    const [ mineData, setMineData  ] = useState<MypageTypes>();
+    const router = useRouter();
+    const [ biddingData, setBiddingData ] = useState<MypageTypes[]>([]);
     const mineItemDatas = [
-        {id: 1, title: "자전거", view: 10, liked: 8, price: 100000, thumbnail: null},
-        {id: 2, title: "인형", view: 2, liked: 5, price: 5000, thumbnail: null},
-        {id: 3, title: "돼지저금통", view: 10, liked: 8, price: 100000, thumbnail: null},
-        {id: 4, title: "목걸이", view: 7, liked: 5, price: 5000, thumbnail: null},
-        {id: 5, title: "귀걸이", view: 109, liked: 8, price: 106500, thumbnail: null},
+        // {id: 1, title: "자전거", view: 10, liked: 8, price: 100000, thumbnail: null},
+        // {id: 2, title: "인형", view: 2, liked: 5, price: 5000, thumbnail: null},
+        {    itemId: 0,
+            title: "자전거",
+            description: "자전거 판매하는 중이다. ",
+            expiredAt: "13",
+            highestPrice: 3000,
+            hopePrice: 50,
+            lowestPrice: 400,
+            itemStatus: "경매중",
+            bidSize: 3,
+        }
     ]
 
     const handleGetMine = async () => {
         try { 
-            const res = await axios.get(`${BASE_URL}/api/items/bid/me`, {
+            //입찰한 상품 
+            const bidRes = await axios.get(`${BASE_URL}/api/items/bid/me`, {
                 headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
             });
 
-            if(res.status === 200){
-                const data = res.data.result.items;
-                setMineData(data);
+            //내가 등록한 상품 
+            const myRes = await axios.get(`${BASE_URL}/api/items/me`)
+
+            if(bidRes.status === 200){
+                const data = bidRes.data.result.items;
+                setBiddingData(data);
+                console.log(data);
+            }
+
+            if(myRes.status === 200){
+                const data = myRes.data.result;
                 console.log(data);
             }
 
@@ -62,18 +80,22 @@ export default function Login() {
             <div className="mt-5">
                 <h1>판매한 상품</h1>
                 <div className="border-b border-t border-LINE_BORDER  h-fit max-w-[800px]">
-                    { mineItemDatas 
+                    { mineItemDatas  && mineItemDatas.length > 0
                         ? mineItemDatas.map((item, idx) => (<MineItem key={idx} data={item} type={"bidded"}/>)) 
-                        : <div className="h-32"></div>
+                        : <div className="h-32 border bg-zinc-50 rounded-lg border-LINE_BORDER my-5">
+                             <div className="text-sm font-normal">등록한 상품이 없습니다.</div>
+                        </div>
                     }
                 </div>
             </div>
             <div className="mt-5">
                 <h1>입찰한 상품</h1>
                 <div className="border-b border-t border-LINE_BORDER h-fit max-w-[800px]">
-                    { mineItemDatas 
-                        ? mineItemDatas.map((item, idx) => (<MineItem key={idx} data={item} type={"sold"}/>)) 
-                        : <div className="h-32"></div>
+                    { biddingData && biddingData.length > 0 
+                        ? biddingData.map((item:MypageTypes, idx:number) => (<MineItem key={idx} data={item} type={"sold"}/>)) 
+                        : <div className="h-32 bg-zinc-100 border rounded-lg border-LINE_BORDER my-5 flex justify-center items-center">
+                            <div className="text-sm font-normal">입찰한 상품이 없습니다.</div>
+                        </div>
                     }
                 </div>
             </div>
