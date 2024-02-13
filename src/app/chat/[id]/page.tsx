856cbcac6c090e132abe348a59297435ac0d81b9
Emma-Hyejin <cjd191058@naver.com>
2024-02-13 15:21:38 +0900

    Feat: websocket + STMP, SockJS

---
+++ src/app/chat/[id]/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+// import ChatListItem from "@/components/chat/ChatListItem";
+// import NewWrite from "../../../public/svg/NewWrite";
+import ChatProductItem from "@/components/chat/ChatProductItem";
+// import ReadChat from "@/components/chat/ReadChat";
+// import SendChat from "@/components/chat/SendChat";
+import SendButton from "../../../../public/svg/SendButton";
+import NewWrite from "../../../../public/svg/NewWrite";
+import { useEffect, useState } from "react";
+import ChatSection from "@/components/chat/ChatSection";
+
+import * as Stomp from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+// import SockJS from 'sockjs-client';
+// import { WebSocket } from 'ws';
+// Object.assign(global, { WebSocket });
+import LocalStorage from "@/util/localstorage";
+// import SendButton from "../../../public/svg/SendButton";
+
+export interface message {
+    message: string;
+    senderId: number;
+    createdAt: string;
+}
+
+
+export interface itemChatInfoTypes {
+    itemId: number;
+    itemTitle: string;
+    itemThumbnailImage: string;
+    highestPrice: number;
+    toUserProfileImage: string;
+}
+
+export interface chatListTypes {
+    chatRoomId: number;
+    toUserName: string;
+    toUserProfileImage: string;
+    lastMessage: string;
+    lastChatTime: string;
+}
+
+export default function Chat() {
+    // const [ messages, setMessages ] = useState<message[]>([]);
+    const accessToken = LocalStorage.getItem("accessToken");
+    const [isConnected, setIsConnected] = useState(false);
+    // const [ socket, setSocket ] = useState<any | null>(null);
+    // const client = useRef({});
+    // console.log(socket);
+
+    // useEffect(() => {
+    //     if(!socket){
+    //         return;
+    //     }
+
+    //     socket.deactivate("disconnect", async () => {
+    //         console.log("==== disconnect ==== ");
+    //         setIsConnected(false);
+    //     });
+    // })    
+
+    useEffect(() => {
+        const socket = new SockJS('http://server.farmingsoon.site/ws', null, {transports: ["websocket", "xhr-streaming", "xhr-polling"]});
+        const client = new Stomp.Client({
+            webSocketFactory: () => socket,
+            connectHeaders: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            debug: (str) => {
+                console.log(`debg: ${str}`)
+            },
+            onConnect: () => {
+                console.log("=== connect Success === ");
+            },
+            onStompError: (frame) => {
+                console.error('Broker reported error: ' + frame.headers['message']);
+                console.error('Additional details: ' + frame.body);
+            },
+        });
+
+        client.activate();
+
+        return () => {client.deactivate();}
+
+        // const client = new Stomp.Client({
+        //     brokerURL: `ws://server.farmingsoon.site/ws`,
+        //     beforeConnect: () => {console.log("before connection")},
+        //     connectHeaders: {
+        //         Authorization: `Bearer ${accessToken}`,
+        //     },
+        //     debug: (str) => {
+        //         console.log(`debug: `, str);
+        //     }, 
+        //     onConnect: () => {
+        //         setIsConnected(true);
+        //         console.log("==== connect ====")
+        //         client.subscribe(`/sub/chat-room/1`, (message) => { 
+        //             console.log(message.body);
+        //         })
+        //     },
+        //     onStompError: (frame) => {
+        //         console.error('Broker reported error: ' + frame.headers['message']);
+        //         console.error('Additional details: ' + frame.body);
+        //     },
+        // });
+
+        // client.activate();
+
+        // return () => {
+        //     client.deactivate();
+        // };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <div className="flex min-h-screen">
+           <div className="w-[371px] border-r border-LINE_BORDER whitespace-nowrap">
+                <div className="flex flex-row justify-between px-3 items-cneter">
+                    <h1 className="font-semibold ">채팅</h1>
+                    <NewWrite width={"20px"} height={"20px"} />
+                </div>
+                <div className="mt-12 border-t border-LINE_BORDER overflow-y-scroll">
+                    {/* <ChatListItem list={undefined} /> */}
+                </div>
+           </div>
+           <div className="flex-grow flex flex-col h-screen">
+                <ChatProductItem />
+                <ChatSection chatRoomId={1} isConnected={isConnected}/>
+                <form className="relative mt-2 px-2">
+                    <input type="text" className="border rounded-lg w-full h-11 px-3 py-1 text-sm font-normal text-TEXT_BLACK" placeholder="메세지를 입력하세요."/>
+                    <span className="absolute right-3 top-2"><SendButton width={"30px"} height={"30px"} /></span>
+                </form>
+           </div>
+        </div>
+    )
+}
--- src/components/chat/ChatProductItem.tsx
+++ src/components/chat/ChatProductItem.tsx
@@ -1,13 +1,26 @@
+import { itemChatInfoTypes } from "@/app/chat/[id]/page";
 import Img from "@/common/Img";
 
-const ChatProductItem = () => {
+interface ChatProducItemTypes {
+    curChatInfo?: itemChatInfoTypes;
+}
+
+const ChatProductItem = ({curChatInfo}:ChatProducItemTypes ) => {
+
+    const formatPrice = (price: number | null | undefined) => {
+        if (price === null) return "0";
+        if (price === undefined) return "0";
+        return String(price).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+
     return(
         <div className="flex flex-row w-full bg-[#F4F4F4] p-3">
-            <Img type={"normal"} src={undefined} width={45} height={45 } />
+            <Img type={"normal"} src={curChatInfo && curChatInfo.itemThumbnailImage} width={45} height={45 } />
             <div className="flex-1 ml-3">
-                <div className="font-normal text-md mb-1">카메라 팝니다. : title</div>
+                <div className="font-normal text-md mb-1">{curChatInfo && curChatInfo.itemTitle}</div>
                 <div className="text-xs font-light">현재 최고가 
-                    <span className="font-normal text-MAIN_COLOR mx-1 ">₩ 100,000</span>원
+                    <span className="font-normal text-MAIN_COLOR mx-1 ">₩ {formatPrice(curChatInfo && curChatInfo.highestPrice)}</span> 원
                 </div>
             </div>
         </div>
