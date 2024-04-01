01f81da37b0163ca4838c8bb3ddd69324fef4efe
Emma-Hyejin <cjd191058@naver.com>
2024-04-01 19:15:05 +0900

    Refactor: axios.interceptors 적용 테스트

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useEffect, useState } from "react"; 
 import { useRecoilState, useRecoilValue } from "recoil";
-import axios from "axios";
+// import axios from "axios";
 
-import { rotateRefresh } from "@/util/axiosCall";
+// import { rotateRefresh } from "@/util/axiosCall";
 import LocalStorage from "@/util/localstorage";
 
 import { tokenState } from "@/stores/tokenModal";
@@ -11,6 +11,8 @@ import { sseNotiAtomFamily } from "@/stores/sseNotiState";
 import { chatListTypes } from "@/types/chat";
 
 import ChatListItem from "@/components/chat/ChatListItem";
+import instance from "@/util/axios_interceptor";
+import { AxiosError } from "axios";
 
 
 export default function Chat() {
@@ -20,63 +22,81 @@ export default function Chat() {
 
   //채팅방 목록 && 채팅 리스트
 
-  const getList = async () => {
-    const url = "https://server.farmingsoon.site/api/chat-rooms/me";
-    const config = { withCredentials: true };
-    const isLogin = LocalStorage.getItem("loginState");
+  // const getList = async () => {
+  //   const url = "https://server.farmingsoon.site/api/chat-rooms/me";
+  //   const config = { withCredentials: true };
+  //   const isLogin = LocalStorage.getItem("loginState");
 
-    if(isLogin === "true"){
-      console.log("  >>> 목록 업데이트 함수 실행 <<< ")
-      try {
-        const res = await axios.get(url, config);
-        if(res.status === 200){
-          setChatList(res.data.result);
-        }
+  //   if(isLogin === "true"){
+  //     console.log("  >>> 목록 업데이트 함수 실행 <<< ")
+  //     try {
+  //       const res = await axios.get(url, config);
+  //       if(res.status === 200){
+  //         setChatList(res.data.result);
+  //       }
 
 
-      } catch (err) {       
-        if(axios.isAxiosError(err) && err.response) {
-          const status = err.response.status;
-          const errorMessage = err.response.data.message;
+  //     } catch (err) {       
+  //       if(axios.isAxiosError(err) && err.response) {
+  //         const status = err.response.status;
+  //         const errorMessage = err.response.data.message;
           
-          if(status === 401){
-            if(errorMessage === "기한이 만료된 AccessToken입니다."){
-              // Access Token expired 
-              console.log("AccessToken 만료");
-              rotateRefresh().catch((refreshErr) => {
-                console.log("ROTATE함수 실행 후 본 컴포넌트 에러 : ", refreshErr)
-                if(refreshErr.message === "RefreshTokenUnauthorized"){
-                  setOpenTokenModal({ tokenExpired: true });
-                  console.log("!! 로테이트 함수의 기한 만료 토큰 관리 !! ")
-                }
-            });
-            }
+  //         if(status === 401){
+  //           if(errorMessage === "기한이 만료된 AccessToken입니다."){
+  //             // Access Token expired 
+  //             console.log("AccessToken 만료");
+  //             rotateRefresh().catch((refreshErr) => {
+  //               console.log("ROTATE함수 실행 후 본 컴포넌트 에러 : ", refreshErr)
+  //               if(refreshErr.message === "RefreshTokenUnauthorized"){
+  //                 setOpenTokenModal({ tokenExpired: true });
+  //                 console.log("!! 로테이트 함수의 기한 만료 토큰 관리 !! ")
+  //               }
+  //           });
+  //           }
       
-            if(errorMessage === "기한이 만료된 RefreshToken입니다."){
-              console.log("RefreshToken 만료로 모달 오픈 ")
-              setOpenTokenModal({ tokenExpired: true });
-            }
+  //           if(errorMessage === "기한이 만료된 RefreshToken입니다."){
+  //             console.log("RefreshToken 만료로 모달 오픈 ")
+  //             setOpenTokenModal({ tokenExpired: true });
+  //           }
               
-          }
+  //         }
       
-          if(status === 403){
-            //로그인 후 이용할 수 있습니다. 
-            setOpenTokenModal({ tokenExpired: true });
-          }
-        };
+  //         if(status === 403){
+  //           //로그인 후 이용할 수 있습니다. 
+  //           setOpenTokenModal({ tokenExpired: true });
+  //         }
+  //       };
         
+  //     }
+  //   }
+
+  //   if(isLogin === "false"){
+  //     console.log("채팅방 === 만료 실행 : ", isLogin)
+  //     setOpenTokenModal({ tokenExpired: true })    
+  //   }
+
+  // };
+  const getList = async () => {
+    try { 
+      const listData = await instance.get("/api/chat-rooms/me");
+      if(listData.status === 200){
+        setChatList(listData.data.result);
+      }
+    } catch (err) {
+      const error = err as AxiosError; // 타입 단언 사용
+      if(error.message === "RefreshTokenExpired"){
+        setOpenTokenModal({ tokenExpired: true });
+      } else {
+        console.log(`목록 함수 에러 ${error}`)
       }
     }
-
-    if(isLogin === "false"){
-      console.log("채팅방 === 만료 실행 : ", isLogin)
-      setOpenTokenModal({ tokenExpired: true })    
-    }
-
-  };
+  }
 
   useEffect(() => {
-    getList(); 
+    const isLogin = LocalStorage.getItem("loginState");
+    if(isLogin === "true"){
+      getList(); 
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
